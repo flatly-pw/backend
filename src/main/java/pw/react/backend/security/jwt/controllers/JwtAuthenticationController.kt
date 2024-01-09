f4bfc96529a438fab41d8078c8a683417d911c89
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-09 20:27:22 +0100

    refactor(controller): Turn JwtAuthenticationController into Kotlin class

--- src/main/java/pw/react/backend/security/jwt/controllers/JwtAuthenticationController.kt
+++ src/main/java/pw/react/backend/security/jwt/controllers/JwtAuthenticationController.kt
@@ -1,55 +1,53 @@
-package pw.react.backend.security.jwt.controllers;
+package pw.react.backend.security.jwt.controllers
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-import pw.react.backend.security.common.AuthenticationService;
-import pw.react.backend.security.common.CommonUserDetailsService;
-import pw.react.backend.security.jwt.models.JwtRequest;
-import pw.react.backend.security.jwt.models.JwtResponse;
-import pw.react.backend.security.jwt.services.JwtTokenService;
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.Valid
+import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import pw.react.backend.security.common.AuthenticationService
+import pw.react.backend.security.common.CommonUserDetailsService
+import pw.react.backend.security.jwt.models.JwtRequest
+import pw.react.backend.security.jwt.models.JwtResponse
+import pw.react.backend.security.jwt.services.JwtTokenService
 
 @RestController
-@RequestMapping(path = JwtAuthenticationController.AUTHENTICATION_PATH)
-@Profile({"jwt"})
-public class JwtAuthenticationController {
-
-    public static final String AUTHENTICATION_PATH = "/auth";
-
-    private final AuthenticationService authenticationService;
-    private final JwtTokenService jwtTokenService;
-    private final CommonUserDetailsService userDetailsService;
-
-    public JwtAuthenticationController(AuthenticationService authenticationService, JwtTokenService jwtTokenService, CommonUserDetailsService userDetailsService) {
-        this.authenticationService = authenticationService;
-        this.jwtTokenService = jwtTokenService;
-        this.userDetailsService = userDetailsService;
+@RequestMapping(JwtAuthenticationController.AUTHENTICATION_PATH)
+@Profile("jwt")
+class JwtAuthenticationController(
+    private val authenticationService: AuthenticationService,
+    private val jwtTokenService: JwtTokenService,
+    private val userDetailsService: CommonUserDetailsService
+) {
+    @PostMapping("/login")
+    fun createAuthenticationToken(
+        @RequestBody authenticationRequest: @Valid JwtRequest,
+        request: HttpServletRequest
+    ): ResponseEntity<*> {
+        authenticationService.authenticate(authenticationRequest.username, authenticationRequest.password)
+        val userDetails = userDetailsService.loadUserByUsername(authenticationRequest.username)
+        val token = jwtTokenService.generateToken(userDetails, request)
+        return ResponseEntity.ok(JwtResponse(token))
     }
 
-    @PostMapping(path = "/login")
-    public ResponseEntity<?> createAuthenticationToken(@Valid @RequestBody JwtRequest authenticationRequest,
-                                                       HttpServletRequest request) throws Exception {
-
-        authenticationService.authenticate(authenticationRequest.username(), authenticationRequest.password());
-        UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.username());
-        String token = jwtTokenService.generateToken(userDetails, request);
-
-        return ResponseEntity.ok(new JwtResponse(token));
-    }
-
-    @PostMapping(path = "/logout")
-    public ResponseEntity<Void> invalidateToken(HttpServletRequest request) {
-        boolean result = jwtTokenService.invalidateToken(request);
-        return result ? ResponseEntity.accepted().build() : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    @PostMapping("/logout")
+    fun invalidateToken(request: HttpServletRequest): ResponseEntity<Void> {
+        val result = jwtTokenService.invalidateToken(request)
+        return if (result) ResponseEntity.accepted().build() else ResponseEntity.status(HttpStatus.BAD_REQUEST).build()
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> removeInvalidTokens() {
-        jwtTokenService.removeTokens();
-        return ResponseEntity.accepted().build();
+    fun removeInvalidTokens(): ResponseEntity<Void> {
+        jwtTokenService.removeTokens()
+        return ResponseEntity.accepted().build()
+    }
+
+    companion object {
+        const val AUTHENTICATION_PATH = "/auth"
     }
 }
