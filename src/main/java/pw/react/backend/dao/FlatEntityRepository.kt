c0e095e8096ee1d6edc6d345628030359cca847b
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-12 18:20:45 +0100

    refactor(repository): make FlatEntityRepository PagingAndSortingRepository

--- src/main/java/pw/react/backend/dao/FlatEntityRepository.kt
+++ src/main/java/pw/react/backend/dao/FlatEntityRepository.kt
@@ -1,6 +1,6 @@
 package pw.react.backend.dao
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import pw.react.backend.models.entity.FlatEntity
 
-interface FlatEntityRepository: JpaRepository<FlatEntity, String>
+interface FlatEntityRepository : PagingAndSortingRepository<FlatEntity, String>
