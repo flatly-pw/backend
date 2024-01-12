603b0cf2d411f16ca0e04d8501ad079d2622615e
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-12 16:55:34 +0100

    feat(domain): Add domain and entity models mappings

--- src/main/java/pw/react/backend/models/domain/Flat.kt
+++ src/main/java/pw/react/backend/models/domain/Flat.kt
@@ -1,5 +1,7 @@
 package pw.react.backend.models.domain
 
+import pw.react.backend.models.entity.FlatEntity
+
 data class Flat(
     val description: String,
     val area: Int,
@@ -8,3 +10,7 @@ data class Flat(
     val capacity: Int,
     val id: String? = null,
 )
+
+fun Flat.toEntity() = FlatEntity(description, area, bedrooms, bathrooms, capacity, id)
+
+fun FlatEntity.toDomain() = Flat(description, area, bedrooms, bathrooms, capacity, id)
