532adc318fc4b5f4ee09d5c5f5b55e80612ebfa6
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-13 18:49:15 +0100

    feat(dto): add FlatDto

---
+++ src/main/java/pw/react/backend/web/FlatDto.kt
@@ -0,0 +1,14 @@
+package pw.react.backend.web
+
+import pw.react.backend.models.domain.Flat
+
+data class FlatDto(
+    val description: String,
+    val area: Int,
+    val bedrooms: Int,
+    val bathrooms: Int,
+    val capacity: Int,
+    val id: String?
+)
+
+fun Flat.toDto() = FlatDto(description, area, bedrooms, bathrooms, capacity, id)
