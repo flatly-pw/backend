bbc42ef49a9044a718a6e2b4110774bbdc2e9c64
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-13 18:42:59 +0100

    feat(swagger): add response documentation for /flat

--- src/main/java/pw/react/backend/controller/FlatController.kt
+++ src/main/java/pw/react/backend/controller/FlatController.kt
@@ -1,5 +1,10 @@
 package pw.react.backend.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,6 +18,14 @@ import pw.react.backend.web.toDto
 @RestController
 class FlatController(private val flatService: FlatService) {
 
+    @Operation(summary = "Get flat offers")
+    @ApiResponse(
+        responseCode = "200",
+        description = "Successfully got flat list. data contains Flat",
+        content = [
+            Content(mediaType = "application/json", schema = Schema(oneOf = [PageDto::class]))
+        ]
+    )
     @GetMapping("/flats")
     fun getAllFlats(@RequestParam page: Int, @RequestParam pageSize: Int): ResponseEntity<*> = try {
         val flatPage = flatService.findAll(PageRequest.of(page, pageSize))
