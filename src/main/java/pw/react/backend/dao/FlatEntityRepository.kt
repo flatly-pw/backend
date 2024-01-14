0eda2c5c7515e05ea986abca5be1f7b8557a757f
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-14 17:32:10 +0100

    feat(repository): Enable FlatEntityRepository to have custom jpa queries

--- src/main/java/pw/react/backend/dao/FlatEntityRepository.kt
+++ src/main/java/pw/react/backend/dao/FlatEntityRepository.kt
@@ -1,6 +1,7 @@
 package pw.react.backend.dao
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.repository.PagingAndSortingRepository
 import pw.react.backend.models.entity.FlatEntity
 
-interface FlatEntityRepository : PagingAndSortingRepository<FlatEntity, String>
+interface FlatEntityRepository : PagingAndSortingRepository<FlatEntity, String>, JpaSpecificationExecutor<FlatEntity>
