924797e2be541eb4c4bdf92af6abf1b39b9e8f13
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-14 19:47:37 +0100

    feat(service): Implement findAll with flat query

--- src/main/java/pw/react/backend/services/FlatService.kt
+++ src/main/java/pw/react/backend/services/FlatService.kt
@@ -1,12 +1,8 @@
 package pw.react.backend.services
 
-import au.com.console.jpaspecificationdsl.and
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.domain.Specification
 import pw.react.backend.dao.FlatEntityRepository
-import pw.react.backend.dao.specification.FlatEntitySpecification.hasBathrooms
-import pw.react.backend.dao.specification.FlatEntitySpecification.hasBedrooms
-import pw.react.backend.dao.specification.FlatEntitySpecification.hasCapacity
 import pw.react.backend.models.domain.FlatQuery
 import pw.react.backend.models.domain.toDomain
 import pw.react.backend.models.entity.FlatEntity
@@ -19,11 +15,16 @@ class FlatService(private val flatEntityRepository: FlatEntityRepository) {
         .findAll(flatSpecification(flatQuery), pageable)
         .map(FlatEntity::toDomain)
 
-    private fun flatSpecification(flatQuery: FlatQuery): Specification<FlatEntity> = with(flatQuery) {
-        and(
-            bedrooms?.let(::hasBedrooms),
-            bathrooms?.let(::hasBathrooms),
-            hasCapacity(adults + children),
-        )
+    private fun flatSpecification(flatQuery: FlatQuery): Specification<FlatEntity> = Specification { root, _, builder ->
+        val predicates = listOf(
+            flatQuery.bedrooms?.let {
+                builder.equal(root.get<Int>("bedrooms"), it)
+            },
+            flatQuery.bathrooms?.let {
+                builder.equal(root.get<Int>("bathrooms"), it)
+            },
+            builder.equal(root.get<Int>("capacity"), flatQuery.adults + flatQuery.children)
+        ).mapNotNull { it }.toTypedArray()
+        builder.and(*predicates)
     }
 }
