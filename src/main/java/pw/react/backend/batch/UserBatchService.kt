73ed97bd0ed1f478368c49943e65843f5103aa7a
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-15 20:38:39 +0100

    refactor(service): Refactor UserBatchService, so it performs operations on domain users

--- src/main/java/pw/react/backend/batch/UserBatchService.kt
+++ src/main/java/pw/react/backend/batch/UserBatchService.kt
@@ -3,6 +3,8 @@ package pw.react.backend.batch
 import org.slf4j.LoggerFactory
 import org.springframework.security.crypto.password.PasswordEncoder
 import pw.react.backend.dao.UserRepository
+import pw.react.backend.models.domain.toDomain
+import pw.react.backend.models.domain.toEntity
 import pw.react.backend.models.entity.UserEntity
 import pw.react.backend.services.UserMainService
 
@@ -13,10 +15,11 @@ class UserBatchService(
 ) : UserMainService(userRepository, passwordEncoder) {
 
     override fun batchSave(users: Collection<UserEntity>): Collection<UserEntity> {
+        val users = users.map { it.toDomain() }
         log.info("Batch insert.")
         return if (users.isNotEmpty()) {
             val insertedUsers = batchRepository.insertAll(
-                users.onEach { it.password = passwordEncoder.encode(it.password) }
+                users.map { it.copy(password = passwordEncoder.encode(it.password)).toEntity() }
             )
             userRepository.findAllByEmailIn(insertedUsers.map(UserEntity::email))
         } else {
