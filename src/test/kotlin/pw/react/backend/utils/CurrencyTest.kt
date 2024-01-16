d075ff68bd6f362d33357aacf3e17c6128965cd5
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-16 18:04:35 +0100

    test: update tests with new conversions

--- src/test/kotlin/pw/react/backend/utils/CurrencyTest.kt
+++ src/test/kotlin/pw/react/backend/utils/CurrencyTest.kt
@@ -11,55 +11,73 @@ class CurrencyTest {
 
     @Test
     fun `Should convert usd to euro properly`() {
-        val usd = 20.0.usd
-        usd.inEuro shouldBe (18.34 plusOrMinus 1e-2)
+        val usd1 = 20.0.usd
+        val usd2 = 20.usd
+        usd1.inEuro shouldBe (18.34 plusOrMinus 1e-2)
+        usd2.inEuro shouldBe (18.34 plusOrMinus 1e-2)
     }
 
     @Test
     fun `Should convert usd to pln properly`() {
-        val usd = 20.0.usd
-        usd.inPln shouldBe (83.33 plusOrMinus 1e-2)
+        val usd1 = 20.0.usd
+        val usd2 = 20.usd
+        usd1.inPln shouldBe (83.33 plusOrMinus 1e-2)
+        usd2.inPln shouldBe (83.33 plusOrMinus 1e-2)
     }
 
     @Test
     fun `Should convert usd to usd properly`() {
-        val usd = 20.0.usd
-        usd.inUsd shouldBe (20.0 plusOrMinus 1e-2)
+        val usd1 = 20.0.usd
+        val usd2 = 20.usd
+        usd1.inUsd shouldBe (20.0 plusOrMinus 1e-2)
+        usd2.inUsd shouldBe (20.0 plusOrMinus 1e-2)
     }
 
     @Test
     fun `Should convert euro to usd properly`() {
-        val euro = 20.0.euro
-        euro.inUsd shouldBe (21.8 plusOrMinus 1e-2)
+        val euro1 = 20.0.euro
+        val euro2 = 20.euro
+        euro1.inUsd shouldBe (21.8 plusOrMinus 1e-2)
+        euro2.inUsd shouldBe (21.8 plusOrMinus 1e-2)
     }
 
     @Test
     fun `Should convert euro to euro properly`() {
-        val euro = 20.0.euro
-        euro.inEuro shouldBe (20.0 plusOrMinus 1e-2)
+        val euro1 = 20.0.euro
+        val euro2 = 20.euro
+        euro1.inEuro shouldBe (20.0 plusOrMinus 1e-2)
+        euro2.inEuro shouldBe (20.0 plusOrMinus 1e-2)
     }
 
     @Test
     fun `Should convert euro to pln properly`() {
-        val euro = 20.0.euro
-        euro.inPln shouldBe (90.83 plusOrMinus 1e-2)
+        val euro1 = 20.0.euro
+        val euro2 = 20.euro
+        euro1.inPln shouldBe (90.83 plusOrMinus 1e-2)
+        euro2.inPln shouldBe (90.83 plusOrMinus 1e-2)
     }
 
     @Test
     fun `Should convert pln to usd properly`() {
-        val pln = 20.0.pln
-        pln.inUsd shouldBe (4.8 plusOrMinus 1e-2)
+        val pln1 = 20.0.pln
+        val pln2 = 20.pln
+        pln1.inUsd shouldBe (4.8 plusOrMinus 1e-2)
+        pln2.inUsd shouldBe (4.8 plusOrMinus 1e-2)
     }
 
     @Test
     fun `Should convert pln to euro properly`() {
-        val pln = 20.0.pln
-        pln.inEuro shouldBe (4.4 plusOrMinus 1e-2)
+        val pln1 = 20.0.pln
+        val pln2 = 20.pln
+        pln1.inEuro shouldBe (4.4 plusOrMinus 1e-2)
+        pln2.inEuro shouldBe (4.4 plusOrMinus 1e-2)
     }
 
     @Test
     fun `Should convert pln to pln properly`() {
-        val pln = 20.0.pln
-        pln.inPln shouldBe (20.0 plusOrMinus 1e-2)
+        val pln1 = 20.0.pln
+        val pln2 = 20.pln
+        pln1.inPln shouldBe (20.0 plusOrMinus 1e-2)
+        pln2.inPln shouldBe (20.0 plusOrMinus 1e-2)
     }
 }
