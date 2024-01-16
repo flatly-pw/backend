ea172907435b8cd2ec9929ac1a68926023d39e0e
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-16 11:13:15 +0100

    feature(dto): add stub data to FlatDto

--- src/main/java/pw/react/backend/web/FlatDto.kt
+++ src/main/java/pw/react/backend/web/FlatDto.kt
@@ -2,14 +2,13 @@ package pw.react.backend.web
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import pw.react.backend.models.domain.Flat
 
 @Serializable
 data class FlatDto(
     val id: String,
     val title: String,
-    val thumbnail: String,
-    val city: String,
-    val rating: Float,
-    @SerialName("price_per_night") val pricePerNight: Float,
+    val thumbnail: String = "https://static.prod.r53.tablethotels.com/media/hotels/slideshow_images_staged/large/1070616.jpg",
+    val city: String = "Warsaw",
+    val rating: Float = 4.7f,
+    @SerialName("price_per_night") val pricePerNight: Float = 999.95f,
 )
