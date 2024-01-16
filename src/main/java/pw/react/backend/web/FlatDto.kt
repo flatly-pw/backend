4d27cdef7789379c640e75d725cffb7c6b19010f
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-16 11:00:50 +0100

    refactor(dto): Fix FlatDto so it contains basic information about the flat

--- src/main/java/pw/react/backend/web/FlatDto.kt
+++ src/main/java/pw/react/backend/web/FlatDto.kt
@@ -1,16 +1,15 @@
 package pw.react.backend.web
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import pw.react.backend.models.domain.Flat
 
 @Serializable
 data class FlatDto(
-    val description: String,
-    val area: Int,
-    val bedrooms: Int,
-    val bathrooms: Int,
-    val capacity: Int,
-    val id: String
+    val id: String,
+    val title: String,
+    val thumbnail: String,
+    val city: String,
+    val rating: Float,
+    @SerialName("price_per_night") val pricePerNight: Float,
 )
-
-fun Flat.toDto() = FlatDto(description, area, bedrooms, bathrooms, capacity, id!!)
