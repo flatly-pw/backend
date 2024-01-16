e6515ba8cfb4f13d476417f325166b80dc027e5f
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-16 18:29:10 +0100

    fix: remove @SerialName from dtos

--- src/main/java/pw/react/backend/web/AddressDto.kt
+++ src/main/java/pw/react/backend/web/AddressDto.kt
@@ -1,12 +1,11 @@
 package pw.react.backend.web
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class AddressDto(
     val street: String = "Krakowskie Przedmieście 42/44",
-    @SerialName("postal_code") val postalCode: String = "00-325",
+    val postalCode: String = "00-325",
     val city: String = "Warsaw",
     val country: String = "Poland",
     val latitude: Double = 21.015868759083475,
--- src/main/java/pw/react/backend/web/ReviewDto.kt
+++ src/main/java/pw/react/backend/web/ReviewDto.kt
@@ -1,12 +1,11 @@
 package pw.react.backend.web
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ReviewDto(
     val rating: Int = 5,
     val review: String = "Great place to stay.",
-    @SerialName("reviewer_name") val reviewerName: String = "Janusz",
+    val reviewerName: String = "Janusz",
     val date: String = "2023-12-12",
 )
