f1dbf0555e2553a473ba424726357cdd7ee8ea27
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-16 15:24:14 +0100

    feat(entity): Add FlatReviewEntity

--- src/main/java/pw/react/backend/models/entity/FlatReviewEntity.kt
+++ src/main/java/pw/react/backend/models/entity/FlatReviewEntity.kt
@@ -2,13 +2,36 @@ package pw.react.backend.models.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
+import jakarta.persistence.EmbeddedId
+import jakarta.persistence.Entity
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
+import java.io.Serializable
+
+@Entity
+@Table(name = "flat_review_entity")
+class FlatReviewEntity(
+    var review: String,
+    var rating: Int,
+    @EmbeddedId var id: FlatReviewId
+) {
+
+    @OneToMany
+    @JoinColumn(name = "flat_entity_id")
+    lateinit var flatEntity: Set<FlatEntity>
+
+    @OneToMany
+    @JoinColumn(name = "user_entity_id")
+    lateinit var userEntity: Set<UserEntity>
+}
 
 @Embeddable
 class FlatReviewId(
     @Column
     var flatId: String,
     var userId: Long
-) {
+) : Serializable {
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
