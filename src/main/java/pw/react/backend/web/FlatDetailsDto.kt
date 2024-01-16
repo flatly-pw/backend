3de6fdac1db15688dae589e02fd1176081d4485f
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-16 09:38:57 +0100

    fix(dto): add thumbnail photo url to gallery

--- src/main/java/pw/react/backend/web/FlatDetailsDto.kt
+++ src/main/java/pw/react/backend/web/FlatDetailsDto.kt
@@ -8,6 +8,7 @@ data class FlatDetailsDto(
     val title: String = "Hotel Bristol",
     val thumbnail: String = "https://lh5.googleusercontent.com/p/AF1QipMv04BhvKSoihsfByh96Pi_-ME7mGQ_RWPMXoyA=w408-h281-k-no",
     val gallery: List<String> = listOf(
+        "https://lh5.googleusercontent.com/p/AF1QipMv04BhvKSoihsfByh96Pi_-ME7mGQ_RWPMXoyA=w408-h281-k-no",
         "https://cf.bstatic.com/xdata/images/hotel/max1024x768/452957266.jpg?k=1aba1f772e5072b81c3b75836062cabc3ee160082eadba004f3a359abdaa17ab&o=&hp=1",
         "https://www.hotelbristolwarsaw.pl/resourcefiles/home-dining-images/belle-epoque.jpg?version=12082023161733",
         "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/2a/e2/26/cafe-bristol-is-a-legendary.jpg?w=700&h=-1&s=1"
