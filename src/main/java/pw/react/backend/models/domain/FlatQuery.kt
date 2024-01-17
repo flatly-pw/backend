5eda2e7422f5b1ef4434f563383391d3dad88c72
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-17 17:58:20 +0100

    refactor: FlatQuery

--- src/main/java/pw/react/backend/models/domain/FlatQuery.kt
+++ src/main/java/pw/react/backend/models/domain/FlatQuery.kt
@@ -3,13 +3,15 @@ package pw.react.backend.models.domain
 import kotlinx.datetime.LocalDate
 
 data class FlatQuery(
+    val city: String?,
+    val country: String?,
     val destinationQuery: DestinationQuery?,
-    val startDate: LocalDate,
-    val endDate: LocalDate,
+    val startDate: LocalDate?,
+    val endDate: LocalDate?,
     val beds: Int?,
     val bedrooms: Int?,
     val bathrooms: Int?,
-    val adults: Int,
-    val children: Int,
-    val pets: Int
+    val adults: Int?,
+    val children: Int?,
+    val pets: Int?
 )
