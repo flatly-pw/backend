f36167d4e3a6dcac93a5f119f13bf0ba26651abf
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-17 17:57:19 +0100

    refactor: define optional requests params instead of body

--- src/main/java/pw/react/backend/controller/FlatController.kt
+++ src/main/java/pw/react/backend/controller/FlatController.kt
@@ -7,14 +7,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import pw.react.backend.services.FlatService
-import pw.react.backend.web.FlatDto
-import pw.react.backend.web.FlatQueryDto
 import pw.react.backend.web.FlatDetailsDto
+import pw.react.backend.web.FlatDto
 import pw.react.backend.web.PageDto
 import pw.react.backend.web.toDto
 
@@ -43,14 +41,19 @@ class FlatController(private val flatService: FlatService) {
     fun getAllFlats(
         @RequestParam page: Int,
         @RequestParam pageSize: Int,
-        @RequestBody(required = false) queryDto: FlatQueryDto?
+        @RequestParam city: String?,
+        @RequestParam country: String?,
+        @RequestParam startDate: String?,
+        @RequestParam endDate: String?,
+        @RequestParam beds: Int?,
+        @RequestParam bedrooms: Int?,
+        @RequestParam bathrooms: Int?,
+        @RequestParam adults: Int?,
+        @RequestParam children: Int?,
+        @RequestParam pets: Int?,
     ): ResponseEntity<*> = try {
         val pageRequest = PageRequest.of(page, pageSize)
-        val flatPage = if (queryDto != null) {
-            flatService.findAll(queryDto.toDomain(), pageRequest)
-        } else {
-            flatService.findAll(pageRequest)
-        }
+        val flatPage = flatService.findAll(pageRequest)
         ResponseEntity.ok(flatPage.toDto { FlatDto(id = it.id!!, title = it.title) })
     } catch (e: IllegalArgumentException) {
         ResponseEntity.badRequest().body(e.message)
