600ae5d2f657c1bd4328e8c9b0fce0851de6686c
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-19 09:22:32 +0100

    refactor: simplify getting FlatDetails in FlatDetailsService

--- src/main/java/pw/react/backend/services/FlatDetailsService.kt
+++ src/main/java/pw/react/backend/services/FlatDetailsService.kt
@@ -3,6 +3,7 @@ package pw.react.backend.services
 import pw.react.backend.dao.FlatEntityRepository
 import pw.react.backend.exceptions.FlatNotFoundException
 import pw.react.backend.models.entity.FlatDetails
+import pw.react.backend.models.entity.toFlatDetails
 import kotlin.jvm.optionals.getOrNull
 
 class FlatDetailsService(private val flatEntityRepository: FlatEntityRepository) {
@@ -10,16 +11,6 @@ class FlatDetailsService(private val flatEntityRepository: FlatEntityRepository)
     fun getFlatDetailsById(id: String): FlatDetails {
         val flatEntity = flatEntityRepository.findById(id).getOrNull()
             ?: throw FlatNotFoundException("Flat with id: $id was not found")
-        return with(flatEntity) {
-            FlatDetails(
-                title = title,
-                area = area,
-                beds = beds,
-                bedrooms = bedrooms,
-                bathrooms = bathrooms,
-                capacity = capacity,
-                description = description,
-            )
-        }
+        return flatEntity.toFlatDetails()
     }
 }
