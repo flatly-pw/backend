6e995cf547bced3e1c7ff5bb11c6df1f269e4a89
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-19 14:02:38 +0100

    feat: add FlatImageService with method to get flat thumbnail

---
+++ src/main/java/pw/react/backend/services/FlatImageService.kt
@@ -0,0 +1,16 @@
+package pw.react.backend.services
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import pw.react.backend.dao.FlatImageRepository
+import pw.react.backend.exceptions.FlatImageException
+
+class FlatImageService(private val flatImageRepository: FlatImageRepository) {
+
+    fun getThumbnailUriByFlatId(flatId: String): String {
+        val thumbnail = flatImageRepository.findThumbnailByFlatId(flatId)
+            ?: throw FlatImageException.ThumbnailNotFound("There was not thumbnail for flatId: $flatId")
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("/flats/$flatId/image/${thumbnail.id.imageId}")
+            .toUriString()
+    }
+}
