e71719eda0745a8e79a60506b36595402cb93592
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-19 14:42:14 +0100

    fix: stub

--- src/test/kotlin/pw/react/backend/stubs/StubFlatImage.kt
+++ src/test/kotlin/pw/react/backend/stubs/StubFlatImage.kt
@@ -6,7 +6,8 @@ import pw.react.backend.models.entity.FlatImageEntity
 fun stubFlatImageEntity(
     flat: FlatEntity,
     id: String = "1",
+    ordinal: Int = 0,
     name: String = "image$id",
     type: String = "jpg",
     bytes: ByteArray = "$name.$type".toByteArray()
-) = FlatImageEntity(flat, name, type, bytes, id)
+) = FlatImageEntity(flat, name, type, bytes, ordinal, id)
