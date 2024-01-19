4da0bcf1c7d6feb8eaeb3af90578589700c41d14
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-19 09:35:07 +0100

    refactor: fix address creation in FlatDetailsService

--- src/main/java/pw/react/backend/services/FlatDetailsService.kt
+++ src/main/java/pw/react/backend/services/FlatDetailsService.kt
@@ -2,6 +2,7 @@ package pw.react.backend.services
 
 import pw.react.backend.dao.FlatEntityRepository
 import pw.react.backend.exceptions.FlatNotFoundException
+import pw.react.backend.models.domain.toDomain
 import pw.react.backend.models.entity.FlatDetails
 import kotlin.jvm.optionals.getOrNull
 
@@ -10,9 +11,7 @@ class FlatDetailsService(private val flatEntityRepository: FlatEntityRepository)
     fun getFlatDetailsById(id: String): FlatDetails {
         val flatEntity = flatEntityRepository.findById(id).getOrNull()
             ?: throw FlatNotFoundException("Flat with id: $id was not found")
-        val address = with(flatEntity.address) {
-            FlatDetails.Address(street, postalCode, city, country, longitude, latitude)
-        }
+        val address = flatEntity.address.toDomain()
         return with(flatEntity) {
             FlatDetails(
                 title = title,
