a75c13e2d62d13b7c93f712f177d2acf110938b4
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-19 11:09:44 +0100

    refactor: fix stubs

--- src/test/kotlin/pw/react/backend/stubs/StubFlatReview.kt
+++ src/test/kotlin/pw/react/backend/stubs/StubFlatReview.kt
@@ -1,5 +1,7 @@
 package pw.react.backend.stubs
 
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.toJavaLocalDate
 import pw.react.backend.models.entity.FlatEntity
 import pw.react.backend.models.entity.FlatReviewEntity
 import pw.react.backend.models.entity.FlatReviewId
@@ -14,4 +16,5 @@ fun stubFlatReviewEntity(
     user: UserEntity = stubUserEntity(id = userId),
     review: String = "This is a fun place to stay at.",
     rating: Int = 5,
-) = FlatReviewEntity(FlatReviewId(flatId, userId), flat, user, review, rating)
+    date: LocalDate = LocalDate(2023, 1, 1)
+) = FlatReviewEntity(FlatReviewId(flatId, userId), flat, user, review, rating, date.toJavaLocalDate())
