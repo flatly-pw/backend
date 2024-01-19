dcc33ba30d4c602833351ebc4325e95aac986d23
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-19 10:38:44 +0100

    chore: add FlatReviewService to di

--- src/main/java/pw/react/backend/services/MainConfig.java
+++ src/main/java/pw/react/backend/services/MainConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pw.react.backend.batch.BatchConfig;
 import pw.react.backend.dao.CompanyLogoRepository;
 import pw.react.backend.dao.FlatEntityRepository;
+import pw.react.backend.dao.FlatReviewRepository;
 import pw.react.backend.models.FlatQueryFactory;
 import pw.react.backend.openapi.OpenApiConfig;
 import pw.react.backend.security.basic.BasicAuthenticationConfig;
@@ -89,6 +90,11 @@ public class MainConfig {
     @Bean
     public FlatService flatService(FlatEntityRepository flatEntityRepository) {
         return new FlatService(flatEntityRepository);
+    }
+
+    @Bean
+    public FlatReviewService flatReviewService(FlatReviewRepository flatReviewRepository) {
+        return new FlatReviewService(flatReviewRepository);
     }
 
     @Bean
