d17bdea7fb0ff6802773d2206ef2c7a6f8c7d1a0
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-19 14:32:50 +0100

    Revert "feat: Add ordinal to FlatImageEntity"
    This reverts commit 4dc0fab934082fbba72a15d91750818c2371b874.

--- src/main/java/pw/react/backend/models/entity/FlatImageEntity.kt
+++ src/main/java/pw/react/backend/models/entity/FlatImageEntity.kt
@@ -1,8 +1,6 @@
 package pw.react.backend.models.entity
 
 import jakarta.persistence.Column
-import jakarta.persistence.Embeddable
-import jakarta.persistence.EmbeddedId
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -11,7 +9,6 @@ import jakarta.persistence.JoinColumn
 import jakarta.persistence.Lob
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
-import java.io.Serializable
 
 @Entity
 @Table(name = "flat_image_entity")
@@ -22,30 +19,5 @@ class FlatImageEntity(
     var fileName: String,
     var fileType: String,
     @Lob @Column(columnDefinition = "MEDIUMBLOB") var bytes: ByteArray,
-    @EmbeddedId var id: FlatImageId,
+    @Id @GeneratedValue(generator = "uuid", strategy = GenerationType.UUID) var id: String
 )
-
-@Embeddable
-class FlatImageId(
-    @Column(name = "image_id") @GeneratedValue(strategy = GenerationType.UUID) var imageId: String? = null,
-    var ordinal: Int
-): Serializable {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as FlatImageId
-
-        if (imageId != other.imageId) return false
-        if (ordinal != other.ordinal) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = imageId.hashCode()
-        result = 31 * result + ordinal
-        return result
-    }
-}
