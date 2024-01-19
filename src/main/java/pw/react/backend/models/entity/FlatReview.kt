ccddad3113228cc8ff4d4f2344488db2f2e17da9
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-19 10:39:28 +0100

    refactor: change date type to LocalDate

--- src/main/java/pw/react/backend/models/entity/FlatReview.kt
+++ src/main/java/pw/react/backend/models/entity/FlatReview.kt
@@ -1,8 +1,10 @@
 package pw.react.backend.models.entity
 
+import kotlinx.datetime.LocalDate
+
 data class FlatReview(
     val rating: Int,
     val review: String,
     val reviewerName: String,
-    val date: String,
+    val date: LocalDate,
 )
