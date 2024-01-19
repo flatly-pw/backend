e4e0fddc18a11ac9f7e3e584ee43674d02e1a5c7
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-19 11:00:34 +0100

    chore: remove default values from ReviewDto

--- src/main/java/pw/react/backend/web/ReviewDto.kt
+++ src/main/java/pw/react/backend/web/ReviewDto.kt
@@ -5,10 +5,10 @@ import pw.react.backend.models.entity.FlatReview
 
 @Serializable
 data class ReviewDto(
-    val rating: Int = 5,
-    val review: String = "Great place to stay.",
-    val reviewerName: String = "Janusz",
-    val date: String = "2023-12-12",
+    val rating: Int ,
+    val review: String,
+    val reviewerName: String,
+    val date: String,
 )
 
 fun FlatReview.toDto() = ReviewDto(rating, review, reviewerName, date.toString())
