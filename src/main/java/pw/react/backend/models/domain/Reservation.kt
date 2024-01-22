94755560d038f9ebbe55f1216c7dcba4e771ab9d
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-22 19:22:17 +0100

    feat: domain and dto Reservation

---
+++ src/main/java/pw/react/backend/models/domain/Reservation.kt
@@ -0,0 +1,44 @@
+package pw.react.backend.models.domain
+
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.toJavaLocalDate
+import kotlinx.datetime.toKotlinLocalDate
+import pw.react.backend.models.entity.FlatEntity
+import pw.react.backend.models.entity.ReservationEntity
+import pw.react.backend.models.entity.UserEntity
+
+data class Reservation(
+    val userId: Long,
+    val flatId: String,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val adults: Int,
+    val children: Int,
+    val pets: Int,
+    val specialRequests: String? = null,
+    val id: Long? = null,
+)
+
+fun Reservation.toEntity(userEntity: UserEntity, flatEntity: FlatEntity) = ReservationEntity(
+    user = userEntity,
+    flat = flatEntity,
+    startDate = startDate.toJavaLocalDate(),
+    endDate = endDate.toJavaLocalDate(),
+    adults = adults,
+    children = children,
+    pets = pets,
+    specialRequests = specialRequests,
+    id = id
+)
+
+fun ReservationEntity.toDomain() = Reservation(
+    userId = user.id!!,
+    flatId = flat.id!!,
+    startDate = startDate.toKotlinLocalDate(),
+    endDate = endDate.toKotlinLocalDate(),
+    adults = adults,
+    children = children,
+    pets = pets,
+    specialRequests = specialRequests,
+    id = id
+)
