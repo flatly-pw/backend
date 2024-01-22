62e268bbdc92afdf58606f1287f8fa3e0b91e473
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-22 19:21:23 +0100

    feat: add method to get user by email

--- src/main/java/pw/react/backend/dao/UserRepository.java
+++ src/main/java/pw/react/backend/dao/UserRepository.java
@@ -3,9 +3,14 @@ package pw.react.backend.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pw.react.backend.models.entity.UserEntity;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
+
+    Optional<UserEntity> findById(Long id);
+
     List<UserEntity> findAllByEmailIn(Collection<String> userNames);
 }
--- src/main/java/pw/react/backend/services/UserService.kt
+++ src/main/java/pw/react/backend/services/UserService.kt
@@ -4,6 +4,7 @@ import pw.react.backend.models.domain.User
 
 
 interface UserService {
+    fun findUserByEmail(email: String): User?
     fun validateAndSave(user: User): User
     fun updatePassword(user: User, password: String): User
     fun saveUnique(user: User): User
