d3090d22af748b2c86b87e8a3bd11b6c634c1037
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-24 09:40:13 +0100

    feat: improve error handling

--- src/main/java/pw/react/backend/controller/ReservationController.kt
+++ src/main/java/pw/react/backend/controller/ReservationController.kt
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import pw.react.backend.exceptions.ReservationException
 import pw.react.backend.security.jwt.services.JwtTokenService
 import pw.react.backend.services.ReservationService
 import pw.react.backend.services.UserService
@@ -25,18 +26,20 @@ class ReservationController(
     fun makeReservation(
         @RequestBody reservationDto: ReservationDto,
         request: HttpServletRequest
-    ): ResponseEntity<*> {
+    ): ResponseEntity<*> = try {
         val token = request.getHeader(HttpHeaders.AUTHORIZATION).substringAfter(BEARER)
         val email = jwtTokenService.getUsernameFromToken(token)
         val userId = userService.findUserByEmail(email)?.id
             ?: throw UsernameNotFoundException("user with email: $email not found")
         val reservation = reservationDto.toDomain(userId)
-        if (!reservationService.canReserveFlat(reservation.flatId, reservation.startDate, reservation.endDate)) {
-            return ResponseEntity.unprocessableEntity()
-                .body("Cannot make a reservation because the flat is already occupied at that term")
-        }
         val savedReservation = reservationService.saveReservation(reservation)
-        return ResponseEntity.ok(savedReservation.toDto())
+        ResponseEntity.ok(savedReservation.toDto())
+    } catch (e: UsernameNotFoundException) {
+        ResponseEntity.badRequest().body(e.message)
+    } catch (e: IllegalArgumentException) {
+        ResponseEntity.badRequest().body(e.message)
+    } catch (e: ReservationException) {
+        ResponseEntity.unprocessableEntity().body(e.message)
     }
 
     companion object {
--- src/main/java/pw/react/backend/services/ReservationService.kt
+++ src/main/java/pw/react/backend/services/ReservationService.kt
@@ -7,6 +7,7 @@ import kotlinx.datetime.toLocalDateTime
 import pw.react.backend.dao.FlatEntityRepository
 import pw.react.backend.dao.ReservationRepository
 import pw.react.backend.dao.UserRepository
+import pw.react.backend.exceptions.ReservationException
 import pw.react.backend.models.domain.Reservation
 import pw.react.backend.models.domain.toDomain
 import pw.react.backend.models.domain.toEntity
@@ -19,7 +20,17 @@ class ReservationService(
     private val timeProvider: TimeProvider,
 ) {
 
-    fun canReserveFlat(flatId: String, startDate: LocalDate, endDate: LocalDate): Boolean {
+    fun saveReservation(reservation: Reservation): Reservation {
+        val anyReservationOverlaps = !canReserveFlat(reservation.flatId, reservation.startDate, reservation.endDate)
+        if (anyReservationOverlaps) throw ReservationException("Cannot make a reservation because term ${reservation.startDate} to ${reservation.endDate} overlaps another reservation")
+        val reservationEntity = reservation.toEntity(
+            userEntity = userRepository.findById(reservation.userId).get(),
+            flatEntity = flatRepository.findById(reservation.flatId).get()
+        )
+        return reservationRepository.save(reservationEntity).toDomain()
+    }
+
+    private fun canReserveFlat(flatId: String, startDate: LocalDate, endDate: LocalDate): Boolean {
         require(startDate < endDate) { "startDate must be earlier than endDate." }
         require(timeProvider().toLocalDateTime(TimeZone.currentSystemDefault()).date <= startDate) { "startDate must not be in the past" }
         val overlappingReservationsCount = reservationRepository.countAllWithOverlappingDates(
@@ -28,15 +39,5 @@ class ReservationService(
             endDate.toJavaLocalDate()
         )
         return overlappingReservationsCount == 0
-    }
-
-    fun saveReservation(reservation: Reservation): Reservation {
-        val anyReservationOverlaps = !canReserveFlat(reservation.flatId, reservation.startDate, reservation.endDate)
-        if (anyReservationOverlaps) throw IllegalArgumentException("Cannot make a reservation because term ${reservation.startDate} to ${reservation.endDate} overlaps another reservation")
-        val reservationEntity = reservation.toEntity(
-            userEntity = userRepository.findById(reservation.userId).get(),
-            flatEntity = flatRepository.findById(reservation.flatId).get()
-        )
-        return reservationRepository.save(reservationEntity).toDomain()
     }
 }
