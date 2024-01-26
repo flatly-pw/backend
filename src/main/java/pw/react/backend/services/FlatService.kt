8fc3b3f0087ace0274291ead01ed280ececb2e6c
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-26 08:22:04 +0100

    feat: handle country and city query

--- src/main/java/pw/react/backend/services/FlatService.kt
+++ src/main/java/pw/react/backend/services/FlatService.kt
@@ -7,6 +7,7 @@ import pw.react.backend.dao.FlatEntityRepository
 import pw.react.backend.models.domain.Flat
 import pw.react.backend.models.domain.FlatQuery
 import pw.react.backend.models.domain.toDomain
+import pw.react.backend.models.entity.AddressEntity
 import pw.react.backend.models.entity.FlatEntity
 
 class FlatService(private val flatEntityRepository: FlatEntityRepository) {
@@ -18,8 +19,17 @@ class FlatService(private val flatEntityRepository: FlatEntityRepository) {
             .map(FlatEntity::toDomain)
     }
 
-    private fun flatSpecification(flatQuery: FlatQuery): Specification<FlatEntity> = Specification { root, _, builder ->
+    private fun flatSpecification(flatQuery: FlatQuery) = Specification<FlatEntity> { root, _, builder ->
+        val destinationPredicates = if (flatQuery.city != null || flatQuery.country != null) {
+            val address = root.join<FlatEntity, AddressEntity>("address")
+            val cityPredicate = flatQuery.city?.let { builder.equal(address.get<String>("city"), it) }
+            val countryPredicate = flatQuery.country?.let { builder.equal(address.get<String>("country"), it) }
+            arrayOf(cityPredicate, countryPredicate)
+        } else {
+            emptyArray()
+        }
         val predicates = listOf(
+            *destinationPredicates,
             flatQuery.beds?.let {
                 builder.equal(root.get<Int>("beds"), it)
             },
