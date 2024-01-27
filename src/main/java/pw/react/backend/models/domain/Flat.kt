1b73fadd78d4a063b7fa64df8d6377123e09d827
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-27 12:39:02 +0100

    feat: improve Flat model

--- src/main/java/pw/react/backend/models/domain/Flat.kt
+++ src/main/java/pw/react/backend/models/domain/Flat.kt
@@ -5,11 +5,15 @@ import pw.react.backend.models.entity.FlatEntity
 data class Flat(
     val title: String,
     val description: String,
+    val thumbnailUrl: String,
     val area: Int,
     val bedrooms: Int,
     val bathrooms: Int,
     val capacity: Int,
     val type: String,
+    val address: Address,
+    val rating: Float,
+    val pricePerNight: Double,
     val id: String? = null,
 )
 
