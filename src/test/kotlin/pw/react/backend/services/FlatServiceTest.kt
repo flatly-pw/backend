5401b9193f62bda565a963642c65536d33479958
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-27 12:50:37 +0100

    test: update FlatSErviceTest

--- src/test/kotlin/pw/react/backend/services/FlatServiceTest.kt
+++ src/test/kotlin/pw/react/backend/services/FlatServiceTest.kt
@@ -23,15 +23,26 @@ class FlatServiceTest {
             findAll(any(), match<PageRequest> { it.pageNumber == 1 && it.pageSize == 1 })
         } returns PageImpl(listOf(stubFlatEntity(id = "2")))
     }
-    private val service = FlatService(repository)
-
+    private val imageService = mockk<FlatImageService>().also {
+        every { it.getThumbnailUriByFlatId("1") } returns "image://flat/1"
+        every { it.getThumbnailUriByFlatId("2") } returns "image://flat/2"
+    }
+    private val reviewService = mockk<FlatReviewService>().also {
+        every { it.getRatingByFlatId("1") } returns 4.7f
+        every { it.getRatingByFlatId("2") } returns 5.0f
+    }
+    private val priceService = mockk<FlatPriceService>().also {
+        every { it.getPriceByFlatId("1") } returns 100.0
+        every { it.getPriceByFlatId("2") } returns 250.0
+    }
+    private val service = FlatService(repository,imageService, reviewService, priceService)
 
     @Test
     fun `findAll returns correct pages`() {
         val page1 = service.findAll(stubFlatQuery(page = 0, pageSize = 1))
         val page2 = service.findAll(stubFlatQuery(page = 1, pageSize = 1))
-        page1 shouldBe PageImpl(listOf(stubFlat(id = "1")))
-        page2 shouldBe PageImpl(listOf(stubFlat(id = "2")))
+        page1 shouldBe PageImpl(listOf(stubFlat(id = "1", pricePerNight = 100.0, rating = 4.7f)))
+        page2 shouldBe PageImpl(listOf(stubFlat(id = "2", pricePerNight = 250.0, rating = 5.0f)))
     }
 
     @Test
