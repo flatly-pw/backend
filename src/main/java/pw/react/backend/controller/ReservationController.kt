fbd53be5f721a4c88b8f0927daa76cf77cc05473
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-28 11:14:23 +0100

    feat: add GET: /reservations endpoint

--- src/main/java/pw/react/backend/controller/ReservationController.kt
+++ src/main/java/pw/react/backend/controller/ReservationController.kt
@@ -8,20 +8,29 @@ import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import pw.react.backend.exceptions.FlatNotFoundException
 import pw.react.backend.exceptions.ReservationException
 import pw.react.backend.security.jwt.services.JwtTokenService
+import pw.react.backend.services.FlatPriceService
+import pw.react.backend.services.FlatService
 import pw.react.backend.services.ReservationService
 import pw.react.backend.services.UserService
+import pw.react.backend.web.PageDto
 import pw.react.backend.web.ReservationDto
+import pw.react.backend.web.UserReservationDto
 import pw.react.backend.web.toDomain
 import pw.react.backend.web.toDto
 
 @RestController
 class ReservationController(
+    private val flatService: FlatService,
     private val reservationService: ReservationService,
+    private val flatPriceService: FlatPriceService,
     private val jwtTokenService: JwtTokenService,
     private val userService: UserService,
 ) {
@@ -66,6 +75,42 @@ class ReservationController(
     } catch (e: IllegalArgumentException) {
         ResponseEntity.badRequest().body(e.message)
     } catch (e: ReservationException) {
+        ResponseEntity.unprocessableEntity().body(e.message)
+    }
+
+    @GetMapping("/reservations")
+    fun getReservations(
+        @RequestParam page: Int,
+        @RequestParam pageSize: Int,
+        request: HttpServletRequest
+    ): ResponseEntity<*> = try {
+        val token = request.getHeader(HttpHeaders.AUTHORIZATION).substringAfter(BEARER)
+        val email = jwtTokenService.getUsernameFromToken(token)
+        val userId = userService.findUserByEmail(email)?.id
+            ?: throw UsernameNotFoundException("user with email: $email not found")
+        val reservationPage = reservationService.getReservations(userId, page, pageSize)
+        val reservationsPageDto: PageDto<List<UserReservationDto>> = reservationPage.toDto { reservation ->
+            with(reservation) {
+                val flat = flatService.findById(flatId)
+                    ?: throw FlatNotFoundException("flat with id: $flatId was not found")
+                val price = flatPriceService.getPriceByFlatId(flatId, startDate, endDate)
+                UserReservationDto(
+                    flatId = flatId,
+                    reservationId = id!!,
+                    title = flat.title,
+                    thumbnailUrl = flat.thumbnailUrl,
+                    city = flat.address.city,
+                    country = flat.address.country,
+                    startDate = startDate.toString(),
+                    endDate = endDate.toString(),
+                    totalPrice = price
+                )
+            }
+        }
+        ResponseEntity.ok().body(reservationsPageDto)
+    } catch (e: IllegalArgumentException) {
+        ResponseEntity.badRequest().body(e.message)
+    } catch (e: FlatNotFoundException) {
         ResponseEntity.unprocessableEntity().body(e.message)
     }
 
