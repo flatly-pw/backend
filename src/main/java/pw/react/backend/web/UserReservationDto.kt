fe2ce3de774c83c0f699ef4c742d14068c9931ad
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-28 11:11:32 +0100

    feat: add dto

---
+++ src/main/java/pw/react/backend/web/UserReservationDto.kt
@@ -0,0 +1,16 @@
+package pw.react.backend.web
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserReservationDto(
+    val flatId: String,
+    val reservationId: Long,
+    val title: String,
+    val thumbnailUrl: String,
+    val city: String,
+    val country: String,
+    val startDate: String,
+    val endDate: String,
+    val totalPrice: Double,
+)
