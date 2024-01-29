f37f6729937f0595c29ad4fbae10330f5d46121f
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-29 22:07:45 +0100

    feat: add endpoint to get unavailable dates

---
+++ src/main/java/pw/react/backend/web/DatePeriodDto.kt
@@ -0,0 +1,12 @@
+package pw.react.backend.web
+
+import kotlinx.serialization.Serializable
+import pw.react.backend.utils.LocalDateRange
+
+@Serializable
+data class DatePeriodDto(
+    val startDate: String,
+    val endDate: String
+)
+
+fun LocalDateRange.toDto() = DatePeriodDto(start.toString(), end.toString())
