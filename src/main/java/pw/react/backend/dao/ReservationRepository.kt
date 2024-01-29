7df56d5d27d7a06580be837aa9eb753f15e81303
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-29 13:32:04 +0100

    feat: add query to get reservations for given flat, month and year

--- src/main/java/pw/react/backend/dao/ReservationRepository.kt
+++ src/main/java/pw/react/backend/dao/ReservationRepository.kt
@@ -47,4 +47,25 @@ interface ReservationRepository : JpaRepository<ReservationEntity, Long> {
         """
     )
     fun findAllCancelledByUserId(userId: Long, pageable: Pageable): Page<ReservationEntity>
+
+    @Query(
+        value = """
+            select reservation from ReservationEntity reservation
+            where reservation.flat.id = ?1 and reservation.cancelled = false and (
+                month(reservation.startDate) = ?2 and year(reservation.startDate) = ?3
+                or
+                month(reservation.endDate) = ?2 and year(reservation.endDate) = ?3
+                or 
+                year(reservation.startDate) < ?3 and year(reservation.endDate) = ?3 and month(reservation.endDate) > ?2
+                or
+                year(reservation.startDate) = ?3 and year(reservation.endDate) > ?3 and month(reservation.startDate) < ?2
+                or
+                year(reservation.startDate) = ?3 and year(reservation.startDate) = year(reservation.endDate) and month(reservation.startDate) < ?2 and month(reservation.endDate) > ?2 
+                or
+                year(reservation.startDate) < ?3 and year(reservation.endDate) > ?3
+            )
+            order by reservation.startDate asc
+        """
+    )
+    fun getReservationsByFlatIdAndMonthYear(flatId: String, month: Int, year: Int): List<ReservationEntity>
 }
