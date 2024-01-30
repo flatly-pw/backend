b00c258498431a7c61f066a60663d9f507e16f5d
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-30 10:48:38 +0100

    feat: add ReservationNotFoundException

--- src/main/java/pw/react/backend/exceptions/ReservationException.kt
+++ src/main/java/pw/react/backend/exceptions/ReservationException.kt
@@ -1,3 +1,5 @@
 package pw.react.backend.exceptions
 
-class ReservationException(override val message: String): Exception(message)
+class ReservationException(override val message: String) : Exception(message)
+
+class ReservationNotFoundException(override val message: String? = null) : Exception(message)
