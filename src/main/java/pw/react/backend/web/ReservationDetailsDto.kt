bc5e88341d8442e0cf94da2ea9e429e414fc81a8
Szymon022 <szymon.kurpas00@gmail.com>
2024-01-30 10:48:28 +0100

    feat: add ReservationDetailsDto

---
+++ src/main/java/pw/react/backend/web/ReservationDetailsDto.kt
@@ -0,0 +1,23 @@
+package pw.react.backend.web
+
+data class ReservationDetailsDto(
+    val reservationId: Long,
+    val flatId: String,
+    val addressDto: AddressDto,
+    val startDate: String,
+    val endDate: String,
+    val phoneNumber: String,
+    val email: String,
+    val clientName: String,
+    val clientLastName: String,
+    val bedrooms: Int,
+    val bathrooms: Int,
+    val beds: Int,
+    val facilities: List<String>,
+    val adults: Int,
+    val children: Int,
+    val pets: Int,
+    val price: Float,
+    // active, passed, cancelled
+    val status: String,
+)
