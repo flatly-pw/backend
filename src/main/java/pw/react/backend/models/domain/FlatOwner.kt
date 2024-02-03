d4199c914f542c23d8e3abd0737cd4706c402314
Aleksander Janek <aleksanderjanek@gmail.com>
2024-02-03 17:32:44 +0100

    toEntity added

--- src/main/java/pw/react/backend/models/domain/FlatOwner.kt
+++ src/main/java/pw/react/backend/models/domain/FlatOwner.kt
@@ -1,7 +1,9 @@
 package pw.react.backend.models.domain
 
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.toJavaLocalDate
 import kotlinx.datetime.toKotlinLocalDate
+import pw.react.backend.models.entity.FlatEntity
 import pw.react.backend.models.entity.FlatOwnerEntity
 
 data class FlatOwner(
@@ -11,6 +13,14 @@ data class FlatOwner(
     val phoneNumber: String,
     val registeredAt: LocalDate,
     val id: Long? = null,
+)
+
+fun FlatOwner.toEntity() = FlatOwnerEntity(
+    name = name,
+    lastName = lastName,
+    email = email,
+    phoneNumber = phoneNumber,
+    registeredAt = registeredAt.toJavaLocalDate(),
 )
 
 fun FlatOwnerEntity.toDomain() = FlatOwner(name, lastName, email, phoneNumber, registeredAt.toKotlinLocalDate(), id!!)
