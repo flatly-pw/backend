e44d43afe0b529e566569303e36dca4be88a3b9b
Szymon022 <szymon.kurpas00@gmail.com>
2024-02-04 13:28:49 +0100

    fix: add missing @Serializable

--- src/main/java/pw/react/backend/web/UpdateUserDataDtos.kt
+++ src/main/java/pw/react/backend/web/UpdateUserDataDtos.kt
@@ -1,5 +1,8 @@
 package pw.react.backend.web
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ChangePasswordDto(
     val currentPassword: String,
     val newPassword: String,
