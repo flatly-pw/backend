58da6d41001b7cdae9acd43e9086c33fa685bd67
Szymon022 <szymon.kurpas00@gmail.com>
2024-02-04 12:56:16 +0100

    clean: remove redundant POST: /users endpoint

--- src/main/java/pw/react/backend/controller/UserController.kt
+++ src/main/java/pw/react/backend/controller/UserController.kt
@@ -1,44 +1,15 @@
 package pw.react.backend.controller
 
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.Schema
-import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import pw.react.backend.exceptions.UserValidationException
-import pw.react.backend.models.domain.User
 import pw.react.backend.services.UserService
-import pw.react.backend.web.UserDto
-import pw.react.backend.web.toDto
 
 @RestController
-@RequestMapping(path = [UserController.USERS_PATH])
+@RequestMapping(UserController.USERS_PATH)
 class UserController(private val userService: UserService) {
-    @Operation(summary = "Create new users")
-    @ApiResponse(
-        responseCode = "201",
-        description = "Users created",
-        content = [
-            Content(mediaType = "application/json", schema = Schema(allOf = [UserDto::class]))
-        ]
-    )
-    @ApiResponse(responseCode = "401", description = "Something went wrong")
-    @PostMapping("")
-    fun createUsers(@RequestBody users: Collection<UserDto>): ResponseEntity<Collection<UserDto>> {
-        return try {
-            val newUsers = userService.batchSave(users.map(UserDto::toDomain)).map(User::toDto)
-            log.info("Password is not going to be encoded")
-            ResponseEntity.status(HttpStatus.CREATED).body(newUsers)
-        } catch (ex: Exception) {
-            throw UserValidationException(ex.message, USERS_PATH)
-        }
-    }
+
+
 
     companion object {
         private val log = LoggerFactory.getLogger(UserController::class.java)
