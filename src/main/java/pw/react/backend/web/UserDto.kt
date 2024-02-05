d8c04662b20eb4447acc3d3298914fc86648a47f
Aleksander Janek <aleksanderjanek@gmail.com>
2024-02-05 17:20:36 +0100

    adding fild authority evrywere elss

--- src/main/java/pw/react/backend/web/UserDto.kt
+++ src/main/java/pw/react/backend/web/UserDto.kt
@@ -2,6 +2,7 @@ package pw.react.backend.web
 
 import jakarta.validation.constraints.Email
 import kotlinx.serialization.Serializable
+import org.springframework.security.core.GrantedAuthority
 import pw.react.backend.models.domain.User
 
 @Serializable
@@ -10,8 +11,9 @@ data class UserDto(
     val name: String,
     val lastName: String,
     val password: String?,
-    val email: @Email String
+    val email: @Email String,
+    val authority: String = "user"
 ) {
 
-    fun toDomain() = User(name, lastName, email, password!!, id)
+    fun toDomain() = User(name, lastName, email, password!!, id, authority)
 }
